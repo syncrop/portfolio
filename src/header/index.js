db54e959fbcd00fb82d55c024c19c6763db2152c
syncrop <albertopuli98@gmail.com>
2022-11-08 22:02:07 +0100

    v1

--- src/header/index.js
+++ src/header/index.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import "./style.css";
 import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
-import { logotext ,socialprofils } from "../content_option";
-import Themetoggle from "../components/themetoggle";
+import { logotext } from "../content_option";
 
 const Headermain = () => {
   const [isActive, setActive] = useState("false");
